bacd670ebbd6666010043c67b35e38dbb8a64983
Klosowski Peter <a1403029@unet.univie.ac.at>
2016-10-25 18:48:51 +0200

    AppType, AppPackage added, AppInterface implemented

---
+++ src/IOTApplication/IOTApplication/AppType.java
@@ -0,0 +1,7 @@
+package IOTApplication.IOTApplication;
+
+public enum AppType {
+	ALARM_CLOCK,
+	COFFEE_MAKER,
+	LIGHT_BULB;
+}
--- src/IOTApplication/IOTApplication/IOTApplicationInterface.java
+++ src/IOTApplication/IOTApplication/IOTApplicationInterface.java
@@ -1,8 +1,14 @@
 package IOTApplication.IOTApplication;
 
 /**
- * Created by melaniebalaz on 24/10/2016.
+ * Interface for all application implementations to include handling of incoming
+ * Notifications as well as sending them to the Subscribers.
+ *
  */
-public class IOTApplicationInterface {
+public interface IOTApplicationInterface {
+	/** Handles a received package and starts an event if triggered. */
+	public void handleIncomingNotification(IOTAppPackage pack);
 
+	/** Sends package to all Subscriber by forwarding it to the client. */
+	public void sendNotificationToSubs(IOTAppPackage pack);
 }
