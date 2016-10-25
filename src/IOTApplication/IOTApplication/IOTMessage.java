e9e4656fd6c89fd9d329aec727e50998ac986bfd
Melanie Balaz <melanie.j.balaz@gmail.com>
2016-10-25 19:36:47 +0200

    application interface ready for use

--- src/IOTApplication/IOTApplication/IOTMessage.java
+++ src/IOTApplication/IOTApplication/IOTMessage.java
@@ -1,23 +1,19 @@
 package IOTApplication.IOTApplication;
 
-import java.net.InetAddress;
-
 /**
  * Class used to package messages between applications ready to be sent.
  * Contains owner IP-Address and type of this application and a message for all the
  * subscribers.
  *
  */
-public class IOTAppPackage {
-	/** Owners IP-Address. */
-	final InetAddress address;
+public class IOTMessage {
+
 	/** Message type set by application. */
 	final String messageType;
 	/** Message for the receivers. */
 	final String message;
 
-	public IOTAppPackage(InetAddress newAddress, String newMessageType, String newMessage) {
-		address = newAddress;
+	public IOTMessage(String newMessageType, String newMessage) {
 		messageType = newMessageType;
 		message = newMessage;
 	}
