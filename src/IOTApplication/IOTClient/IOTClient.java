7d3d75bd4e82d02706a98f2d3010c2c9f29a0edc
Melanie Balaz <melanie.j.balaz@gmail.com>
2016-10-28 11:16:55 +0200

    createSubscriptionRequest in Client implemented

--- src/IOTApplication/IOTClient/IOTClient.java
+++ src/IOTApplication/IOTClient/IOTClient.java
@@ -2,6 +2,11 @@ package IOTApplication.IOTClient;
 
 import IOTApplication.IOTApplication.IOTMessage;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by melaniebalaz on 28/10/2016.
  */
@@ -12,9 +17,24 @@ public class IOTClient implements IOTClientInterface {
     }
     public void notifySubscribers(IOTMessage message){
         //Send HTTP Post Request
+        
 
     }
     public void createSubscriptionRequest(String destinationIP, int destinationPort){
-        //Send HTTP GET Request
+        //Send HTTP GET Request to the passed IP and Port
+
+        HttpURLConnection newConnection;
+        try {
+            URL url = new URL("http://" + destinationIP + ":" + destinationPort + "/"); //Set the custom URL
+            newConnection = (HttpURLConnection)url.openConnection(); //Open the connection
+            newConnection.setRequestMethod("GET"); //Set the request type
+
+        }
+        catch (MalformedURLException exception){
+
+        }
+        catch (IOException exception){
+
+        }
     }
 }
