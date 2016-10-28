6087f3a24ac9f15fbcc7a33b48c0bd6da96ba5ce
Mai <emily.maitan@gmail.com>
2016-10-28 21:37:57 +0200

    add debugging main method
    ! removed forwarding to application for debugging purposes

--- src/IOTApplication/IOTServer/UDPListener/UDPListener.java
+++ src/IOTApplication/IOTServer/UDPListener/UDPListener.java
@@ -4,9 +4,7 @@ import IOTApplication.IOTServer.IOTServerInterface;
 
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.*;
 
 /**
  * Created by Mai on 10/26/2016.
@@ -43,13 +41,14 @@ public class UDPListener implements UDPListenerInterface {
                 System.out.println("Received offering from " + datagram.getAddress());
                 System.out.println(data);
 
-                receiverServer.receiveServiceOffering(datagram.getAddress(), data);
+                //receiverServer.receiveServiceOffering(datagram.getAddress(), data);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
         } finally {
+            if (socket != null) socket.close();
             this.running = false;
         }
     }
@@ -57,7 +56,7 @@ public class UDPListener implements UDPListenerInterface {
     @Override
     public void terminate() {
         this.running = false;
-        socket.close();
+        if (socket != null) socket.close();
     }
 
     public Integer getPort() {
@@ -66,5 +65,39 @@ public class UDPListener implements UDPListenerInterface {
 
     public boolean isRunning() {
         return this.running;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return "Listener status: " + running + "\n" +
+                    "Location: " + InetAddress.getLocalHost() + ":" + port;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return "Broadcast status: " + running + " at port " + port;
+    }
+
+    /**
+     * Debug method.
+     * @param args No arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Debugging Listener");
+        UDPListenerInterface iListen = new UDPListener(1000, null);
+
+        Thread listenerThread = new Thread (iListen);
+        listenerThread.start();
+
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        iListen.terminate();
+        System.exit(0);
     }
 }
