ee377a2dd7586cf59553690ae70f15de6598e09d
Melanie Balaz <melanie.j.balaz@gmail.com>
2016-10-28 11:29:20 +0200

    half implemented post, need to fetch the current client stuff

--- src/IOTApplication/IOTClient/IOTClient.java
+++ src/IOTApplication/IOTClient/IOTClient.java
@@ -1,25 +1,62 @@
 package IOTApplication.IOTClient;
 
 import IOTApplication.IOTApplication.IOTMessage;
+import IOTApplication.Subscriber;
+import IOTApplication.SubscriberList;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by melaniebalaz on 28/10/2016.
  */
 public class IOTClient implements IOTClientInterface {
 
+    SubscriberList subscribers;
+
+    public IOTClient (SubscriberList pSubscribers){
+        subscribers = pSubscribers;
+    }
+
     public void broadcastServiceOffering(){
         //make a UDP broadcast
     }
     public void notifySubscribers(IOTMessage message){
-        //Send HTTP Post Request
-        
+        //Create a gson object and wrap it into json
+
+
+        //Send HTTP Post Request to each subscriber in the list
+        ArrayList<Subscriber> list;
+        list = subscribers.getSubscribers();
+
+        for (Subscriber subcriber: list) {
+            String url = createUrl(subcriber.getIpAddress(),subcriber.getPort());
+            createNewPostRequest(url);
+        }
 
     }
+
+    private String createUrl(String ipAddress,  int port){
+        return("http://" + ipAddress + ":" + port + "/");
+    }
+
+    public void createNewPostRequest(String urlString){
+        HttpURLConnection newConnection;
+        try {
+            URL url = new URL(urlString); //Set the custom URL
+            newConnection = (HttpURLConnection) url.openConnection(); //Open the connection
+            newConnection.setRequestMethod("POST"); //Set the request type
+
+        } catch (MalformedURLException exception) {
+
+        } catch (IOException exception) {
+
+        }
+    }
+
     public void createSubscriptionRequest(String destinationIP, int destinationPort){
         //Send HTTP GET Request to the passed IP and Port
 
