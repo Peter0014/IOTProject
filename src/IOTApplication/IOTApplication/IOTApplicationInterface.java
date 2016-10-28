93ed27137165ec23e7e8356b98fc6112e330fe26
Peter Klosowski <a1403029@unet.univie.ac.at>
2016-10-28 12:37:36 +0200

    Update classes with methods for creating, updating, getting and deleting data sets.

--- src/IOTApplication/IOTApplication/IOTApplicationInterface.java
+++ src/IOTApplication/IOTApplication/IOTApplicationInterface.java
@@ -2,10 +2,22 @@ package IOTApplication.IOTApplication;
 
 /**
  * Interface for all application implementations to include handling of incoming
- * Notifications as well as sending them to the Subscribers.
+ * Notifications as well as creating, updating, getting and deleting data sets.
  *
  */
 public interface IOTApplicationInterface {
 	/** Handles a received package and starts an event if triggered. */
 	public void handleIncomingNotification(IOTMessage message);
+	
+	/** Returns the current state of previously added data entries. */
+	public String getDataSet();
+	
+	/** Adds a new entry to the application object */
+	public int createDataEntry(String data);
+
+	/** Updates an existing entry in the application object */
+	public int updateDataEntry(String data);
+
+	/** Deletes an existing entry in the application object */
+	public int deleteDataEntry(String data);
 }
