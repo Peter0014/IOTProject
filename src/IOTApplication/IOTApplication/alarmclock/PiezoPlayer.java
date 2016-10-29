aac579e064cdebbaf22ea9bac27a9db9e3125afd
Klosowski Peter <a1403029@unet.univie.ac.at>
2016-10-29 16:05:18 +0200

    Added Source

--- src/IOTApplication/IOTApplication/alarmclock/PiezoPlayer.java
+++ src/IOTApplication/IOTApplication/alarmclock/PiezoPlayer.java
@@ -2,83 +2,55 @@ package IOTApplication.IOTApplication.alarmclock;
 
 import com.pi4j.wiringpi.SoftTone;
 
+/**
+ * 
+ * Sources: Mario Theme: http://www.princetronics.com/supermariothemesong/ on
+ * the 28th Oct 2016
+ * 
+ * @author peter
+ *
+ */
 public class PiezoPlayer {
-	
+
 	private final int PIEZO_PIN = 0;
-	
+
 	static {
 		com.pi4j.wiringpi.Gpio.wiringPiSetup();
 	}
-	
-	private final int[][] MARIO_THEME = 
-		{ 
-			{ 
-				2637, 2637, 0, 2637,
-				0, 2093, 2637, 0,
-				3136, 0, 0, 0,
-				1568, 0, 0, 0,
-				
-				2093, 0, 0, 1568,
-				0, 0, 1319, 0,
-				0, 1760, 0, 1976,
-				0, 1865, 1760, 0,
 
-				1568, 2637, 3136,
-				3520, 0, 2794, 3136,
-				0, 2637, 0, 2093,
-				2349, 1976, 0, 0,
+	private final int[][] MARIO_THEME = { { 2637, 2637, 0, 2637, 0, 2093, 2637, 0, 3136, 0, 0, 0, 1568, 0, 0, 0,
 
-				2093, 0, 0, 1568,
-				0, 0, 1319, 0,
-				0, 1760, 0, 1976,
-				0, 1865, 1760, 0,
+			2093, 0, 0, 1568, 0, 0, 1319, 0, 0, 1760, 0, 1976, 0, 1865, 1760, 0,
 
-				1568, 2637, 3136,
-				3520, 0, 2794, 3136,
-				0, 2637, 0, 2093,
-				2349, 1976, 0, 0
-			},
-			
-			{ 
-				12, 12, 12, 12,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
+			1568, 2637, 3136, 3520, 0, 2794, 3136, 0, 2637, 0, 2093, 2349, 1976, 0, 0,
 
-				12, 12, 12, 12,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
+			2093, 0, 0, 1568, 0, 0, 1319, 0, 0, 1760, 0, 1976, 0, 1865, 1760, 0,
 
-				9, 9, 9,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
+			1568, 2637, 3136, 3520, 0, 2794, 3136, 0, 2637, 0, 2093, 2349, 1976, 0, 0 },
 
-				12, 12, 12, 12,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
+			{ 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
 
-				9, 9, 9,
-				12, 12, 12, 12,
-				12, 12, 12, 12,
-				12, 12, 12, 12, }
-		};
-	
+					12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+
+					9, 9, 9, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+
+					12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+
+					9, 9, 9, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, } };
+
 	public void playTune(int melody) {
 		if (melody < 0) {
 			return;
 		}
-		int [][] tune;
+		int[][] tune;
 		switch (melody) {
-		default: 
-		case 0: 
+		default:
+		case 0:
 			tune = MARIO_THEME;
 			break;
 		}
 		SoftTone.softToneCreate(PIEZO_PIN);
-		
+
 		for (int i = 0; i < tune[0].length; i++) {
 			System.out.println("Sounding now: " + tune[0][i]);
 			SoftTone.softToneWrite(PIEZO_PIN, tune[0][i]);
@@ -97,6 +69,6 @@ public class PiezoPlayer {
 		SoftTone.softToneWrite(PIEZO_PIN, 0);
 		SoftTone.softToneStop(PIEZO_PIN);
 		System.out.println("Finished now!");
-		
+
 	}
 }
