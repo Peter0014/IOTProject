11bf8b8d2b8ad4ece44af1f34bae71982b77593c
Mai <emily.maitan@gmail.com>
2016-11-27 22:18:57 +0100

    add wrapper to method interface

--- src/IOT/IOT_SOAP/IACSoapService.java
+++ src/IOT/IOT_SOAP/IACSoapService.java
@@ -5,6 +5,8 @@ import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
 import java.util.ArrayList;
@@ -30,6 +32,7 @@ public interface IACSoapService {
     @RequestWrapper(className = "java.lang.String")
     @ResponseWrapper(className = "java.util.HashMap")
     @WebResult(name="alarm")
+    @XmlJavaTypeAdapter(LongStringHashmapAdapter.class)
     public HashMap<Long, String> getAlarm(@WebParam(name="msDate", mode= WebParam.Mode.IN) String msDate);
 
     @WebMethod(operationName = "postAlarm", exclude = false)
